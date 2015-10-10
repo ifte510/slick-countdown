2da22d0f5043b9faa98215b1ffbf3e5a44c658d2
ifte.hsn <ifte.hsn@gmail.com>
2015-10-10 08:58:54 +0600

    slick-sc.php file deleted

--- slick-count-down.php
+++ slick-count-down.php
@@ -72,10 +72,6 @@ if(!class_exists('Slick_CountDown_Core')){
             wp_enqueue_style( 'slick-popup', SLICK_PLUGIN_URI . '/css/popup.css', false, Slick_CountDown_Core::VERSION, 'all' );
             wp_enqueue_style( 'jquery-ui-timepicker-addon', SLICK_PLUGIN_URI . '/css/jquery-ui-timepicker-addon.css', false, Slick_CountDown_Core::VERSION, 'all' );
 
-
-
-
-
             wp_enqueue_script( 'wp-color-picker' );
             wp_enqueue_script( 'jquery-ui-core' );
             wp_enqueue_script( 'jquery-ui-datepicker' );
